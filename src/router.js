dd050cf14183c9296e28beac03740bc93c1726e9
Jeffrey Wong <j.wong@greenvalley.nl>
2021-11-16 16:41:35 +0100

    Optimized components for deployment

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachesList from '@/pages/coaches/CoachesList';
-import CoachesDetail from '@/pages/coaches/CoachesDetail';
-import CoachesRegistration from '@/pages/coaches/CoachesRegistration';
-import ContactCoach from '@/pages/requests/ContactCoach';
-import RequestsReceived from '@/pages/requests/RequestsReceived';
-import UserAuth from '@/pages/auth/UserAuth';
 import store from '@/store/index';
 import NotFound from '@/pages/NotFound';
 
+const CoachesDetail = () => import('@/pages/coaches/CoachesDetail');
+const CoachesRegistration = () => import('@/pages/coaches/CoachesRegistration');
+const ContactCoach = () => import('@/pages/requests/ContactCoach');
+const RequestsReceived = () => import('@/pages/requests/RequestsReceived');
+const UserAuth = () => import('@/pages/auth/UserAuth');
 
 const router = createRouter({
   history: createWebHistory(),
